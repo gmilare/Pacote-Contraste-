8c38b8fe0a6a48e9b68e8083b6200898e6dbaa81
Gisele Milare <70180982+gmilare@users.noreply.github.com>
2021-06-12 11:09:58 -0300

    Update example.py

--- contraste/examples/example.py
+++ contraste/examples/example.py
@@ -6,4 +6,25 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 #
 
-"""Example for Contraste."""
+##Instalação do pacote gdal como pré-requisito do pacote 
+from osgeo import gdal
+
+##instalação do pacote
+pip install contraste-1.0.tar.gz
+
+##importar a função de contrastre raiz quadrada do pacote
+#outras funções disponíveis: 
+from contraste import RaizQuadrada
+
+##Chamada da função
+RaizQuadrada()
+# A função listará os arquivos .tif do diretório raiz. Selecionar a imagem (Landsat 8) de acordo com a numeração.
+# O contraste será aplicado na imagem selecionada
+
+####importar a função de contrastre por classe
+from contraste import PorClasse
+
+##Chamada da função
+PorClasse()
+# A função listará os arquivos .tif do diretório raiz. Selecionar a imagem (Landsat 8) de acordo com a numeração e, posteriormente, a imagem classificada (Mapbiomas).
+# Escolha se o contraste será otimizado (1) ou personalizado (2)
