a7400043cc2a37dae7072b960961ac18283ff728
Gisele Milare <70180982+gmilare@users.noreply.github.com>
2021-06-15 11:09:19 -0300

    Delete contraste/dist/contraste-1.0/contraste-1.0 directory

--- contraste/dist/contraste-1.0/contraste-1.0/CHANGES.rst
+++
@@ -1,4 +0,0 @@
-Versão 1.0.0
-------------
-
-- Primeira versão estável do pacote
--- contraste/dist/contraste-1.0/contraste-1.0/contraste/__init__.py
+++
@@ -1,3 +0,0 @@
-from .contraste import Equalizacao, Linear, MinMax, Negativo, PorClasse, Quadratico, RaizQuadrada
-
-__all__ = ["Equalizacao", "Linear", "MinMax", "Negativo", "PorClasse", "Quadratico", "RaizQuadrada"]
